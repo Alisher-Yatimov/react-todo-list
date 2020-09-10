b281a761e359d24faa70e6bb4fdad976d8a393e7
Alisher <23ali0998@gmail.com>
2020-09-10 12:43:30 +0300

    Updates

---
+++ precache-manifest.d9a087c7448bdca4c41df50a44562ac2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068e89c285dc6a71a4a4684154fff877",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "4d52464e75ee4c9f79bf",
+    "url": "/react-todo-list/static/css/main.b6694b84.chunk.css"
+  },
+  {
+    "revision": "d9ca127d0beef87667ea",
+    "url": "/react-todo-list/static/js/2.98e0e3a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-list/static/js/2.98e0e3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d52464e75ee4c9f79bf",
+    "url": "/react-todo-list/static/js/main.6ca91aec.chunk.js"
+  },
+  {
+    "revision": "5ad088ed5fdd1b805caa",
+    "url": "/react-todo-list/static/js/runtime-main.700c6f01.js"
+  },
+  {
+    "revision": "fd0c3d2b6a77b086f8695afdc87874b9",
+    "url": "/react-todo-list/static/media/notepad.fd0c3d2b.svg"
+  }
+]);
